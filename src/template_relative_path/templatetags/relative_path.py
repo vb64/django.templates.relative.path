44947b564bf04764b087c2f516900cddb9e6d05d
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-03-16 00:16:41 +0300

    inject into django template code

--- src/template_relative_path/templatetags/relative_path.py
+++ src/template_relative_path/templatetags/relative_path.py
@@ -1,16 +1,50 @@
 from django.conf import settings
 from django.template.loaders import filesystem as fs, app_directories as ad
-from django.template.loader_tags import ExtendsNode
-from django.template.base import Template, TemplateSyntaxError
+from django.template.loader_tags import ExtendsNode as ExtendsNodeParent
+from django.template.base import TemplateSyntaxError, TemplateEncodingError, StringOrigin, Lexer, Parser, Template as TemplateParent
+from django.utils.encoding import smart_unicode
+
+def compile_string(template_string, origin, name):
+    "Compiles template_string into NodeList ready for rendering"
+    if settings.TEMPLATE_DEBUG:
+        from django.template.debug import DebugLexer, DebugParser
+        lexer_class, parser_class = DebugLexer, DebugParser
+    else:
+        lexer_class, parser_class = Lexer, Parser
+    lexer = lexer_class(template_string, origin)
+    parser = parser_class(lexer.tokenize())
+    parser.template_name = name
+    return parser.parse()
+
+class Template(TemplateParent):
+    def __init__(self, template_string, origin=None, name=None, engine=None):
+        try:
+            template_string = smart_unicode(template_string)
+        except UnicodeDecodeError:
+            raise TemplateEncodingError("Templates can only be constructed "
+                                        "from unicode or UTF-8 strings.")
+        if settings.TEMPLATE_DEBUG and origin is None:
+            origin = StringOrigin(template_string)
+        self.nodelist = compile_string(template_string, origin, name)
+        self.name = name
+        self.origin = origin
+
+        # !!! ATTENTION !!!
+        # for django.VERSION > 1.4
+        # always use default engine for render
+        try:
+            from django.template.engine import Engine
+            self.engine = Engine.get_default()
+        except:
+            pass
 
 class filesystem(fs.Loader):
     is_usable = True
 
     def load_template(self, template_name, template_dirs=None):
         source, origin = self.load_template_source(template_name, template_dirs)
-        print "\nfilesystem source:\n\n'%s'\n\norigin: '%s'\ntemplate_name: '%s'\n\n" % (source, origin, template_name)
-
-        template = Template(source)
+        #print "\nfilesystem source:\n\n'%s'\n\norigin: '%s'\ntemplate_name: '%s'\n\n" % (source, origin, template_name)
+        template = Template(source, name=template_name)
         return template, origin
 
 class app_directories(ad.Loader):
@@ -18,22 +52,26 @@ class app_directories(ad.Loader):
 
     def load_template(self, template_name, template_dirs=None):
         source, origin = self.load_template_source(template_name, template_dirs)
-        print "\napp_directories source:\n\n'%s'\n\norigin:\n\n'%s'\n\n" % (source, origin)
-
-        template = Template(source)
+        template = Template(source, name=template_name)
         return template, origin
 
 from django import template
 register = template.Library()
 
+# !!! ATTENTION !!!
+# disable rule, that 'extends' must be first tag in template
+# this need for {% load %} tag
+class ExtendsNode(ExtendsNodeParent):
+    must_be_first = False
+
 @register.tag('extends')
 def do_extends(parser, token):
-
-    print "\ndo_extends token: '%s'\n" % token
-
     bits = token.split_contents()
     if len(bits) != 2:
         raise TemplateSyntaxError("'%s' takes one argument" % bits[0])
+
+    print "\ndo_extends: '%s' template_name: '%s'" % (bits[1], parser.template_name)
+
     parent_name = parser.compile_filter(bits[1])
     nodelist = parser.parse()
     if nodelist.get_nodes_by_type(ExtendsNode):
