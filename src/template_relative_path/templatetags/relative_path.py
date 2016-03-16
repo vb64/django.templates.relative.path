a3968062fc44c856edf354e8630d213473a6ed69
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-03-16 11:54:45 +0300

    implement relative path construction

--- src/template_relative_path/templatetags/relative_path.py
+++ src/template_relative_path/templatetags/relative_path.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.template.loaders import filesystem as fs, app_directories as ad
 from django.template.loader_tags import ExtendsNode as ExtendsNodeParent
@@ -63,14 +65,38 @@ register = template.Library()
 class ExtendsNode(ExtendsNodeParent):
     must_be_first = False
 
+def construct_relative_path (name, relative_name):
+    if not relative_name.startswith('"'):
+        # argument is variable
+        return relative_name
+
+    levels = -1
+    for ch in relative_name[1:]:
+        if ch == '.':
+            levels += 1
+        else:
+            break
+
+    if (not name) or (levels < 0):
+        # relative_name not starts with '.'
+        return relative_name
+
+    folders = os.path.dirname(name).split('/')
+    if levels > len(folders):
+        raise TemplateSyntaxError("Relative name '%s' have more parent folders, then given name '%s'" % (relative_name, name))
+
+    result = folders[:len(folders) - levels]
+    result.append(relative_name[levels+2:-1])
+    return '"%s"' % '/'.join(result)
+
 @register.tag('extends')
 def do_extends(parser, token):
     bits = token.split_contents()
     if len(bits) != 2:
         raise TemplateSyntaxError("'%s' takes one argument" % bits[0])
 
-    #print "\ndo_extends: '%s' template_name: '%s'" % (bits[1], parser.template_name)
-    parent_name = parser.compile_filter(bits[1])
+    #print "\ndo_extends: '%s' template_name: '%s' result: '%s'" % (bits[1], parser.template_name, n)
+    parent_name = parser.compile_filter(construct_relative_path(parser.template_name, bits[1]))
     nodelist = parser.parse()
     if nodelist.get_nodes_by_type(ExtendsNode):
         raise TemplateSyntaxError("'%s' cannot appear more than once in the same template" % bits[0])
