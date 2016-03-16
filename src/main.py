023da48e837b86ade7544a0d6fcd04a15694da27
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-03-16 13:04:05 +0300

    doctest

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+"""
+Test script for django relative_path module
+(C) 2016 by Vitaly Bogomolov mail@vitaly-bogomolov.ru
+
+>>> rend('subdir/sub2.html')
+u'Base1. sub2 content'
+>>> rend('subdir/sub3.html')
+u'Base1. sub33 content'
+>>> rend('subdir/subdir2/sub3.html')
+u'Base1. sub3 content'
+>>> rend('subdir/subdir2/sub4.html')
+u'Base2. sub4 content'
+"""
 import os
 
 import django
@@ -23,16 +36,30 @@ settings.configure(
         'template_relative_path',
     ),
 
+    # for django version > 1.4
+#    TEMPLATES = [{
+#        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#        'DIRS': [os.path.join(ROOT_PROJECT, 'tpl').replace('\\', '/')],
+#        'OPTIONS': {
+#            'loaders': [
+#                'template_relative_path.templatetags.relative_path.filesystem',
+#                'template_relative_path.templatetags.relative_path.app_directories',
+#            ],
+#        },
+#    }]
 )
 
+#print "django version", django.VERSION
 django.setup()
-#print django.VERSION
 
 from django import template
 from django.template.loader import get_template
 
-tpl_file = 'subdir/sub2.html'
-t = get_template(tpl_file)
 c = template.Context({})
 
-print t.render(c)
+def rend(template):
+    return get_template(template).render(c)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
